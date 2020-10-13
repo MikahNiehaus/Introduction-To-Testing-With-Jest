5b0f7d03facceea3a7bbfbb769b2aaaf2eae614c
Mikah Niehaus <Graywolf.niehaus@live.com>
2020-10-13 01:36:22 -0500

    basic testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,13 @@
-test('Fake Test', () => {
-    expect(true).toBeTruthy();
+import {add, total} from './App';
+
+
+test('add', () => {
+    expect(add(1,2)).toBe(3);
+    expect(add(-2,2)).toBe(0);
+    expect(add(-1,-2)).toBe(-3);
+
+})
+
+test('total', () => {
+    expect(total(5, 20)).toBe("$25")
 })
